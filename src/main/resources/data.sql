12677b69bc6da50a3c902e58c3731b2a707ef81e
EzefcDev <ezeferrauti@gmail.com>
2022-11-13 10:02:59 -0300

    Change data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,9 +12,9 @@ create table message( message_id int not null auto_increment, user_dni varchar(2
        ('Patricio', 'Velzaques', '33555666');
 
  insert into message ( user_dni, target_user, message_text, viewed)
-  values('33227034', '22408890', 'hola', false),
-        ('33555666', '33227034', 'que haces', false),
+  values('20437037', '22408890', 'hola', false),
+        ('33555666', '20437037', 'que haces', false),
         ('33227034', '22408890', 'estoy en la luna', false),
-        ('22408890', '33227034', 'wacho loco', false),
+        ('22408890', '20437037', 'wacho loco', false),
         ('33555666', '22408890', 'mira vos', false),
-        ('33555666', '33227034', 'va como pinia', false);
+        ('33555666', '20437037', 'va como pinia', false);
